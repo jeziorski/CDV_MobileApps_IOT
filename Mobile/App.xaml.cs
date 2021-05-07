8fda1a5df76ad202516739199332b9acb8739afb
jeziorski <48910840+jeziorski@users.noreply.github.com>
2021-05-07 18:59:16 +0200

    Old repos added

---
+++ Mobile/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobile_App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Mobile/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Essentials;
+
+namespace mobile_App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public class MainPage : ContentPage
+    {
+        string translatedNumber;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            void OnTranslate(object sender, EventArgs e)
+            {
+                string enteredNumber = phoneNumberText.Text;
+                translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+                if (!string.IsNullOrEmpty(translatedNumber))
+                {
+                    callButton.IsEnabled = true;
+                    callButton.Text = "Call " + translatedNumber;
+                }
+                else
+                {
+                    callButton.IsEnabled = false;
+                    callButton.Text = "Call";
+                }
+            }
+            async void OnCall(object sender, System.EventArgs e)
+            {
+                if (await this.DisplayAlert(
+                        "Dial a Number",
+                        "Would you like to call " + translatedNumber + "?",
+                        "Yes",
+                        "No"))
+                {
+                    // TODO: dial the phone
+                }
+            }
+        }
+    }
+}
